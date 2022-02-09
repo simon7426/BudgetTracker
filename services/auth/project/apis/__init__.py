5ce08ec9d0913f404f260c42e7b2c4af16e35559
simon.islam <simon.islam@brilliant.com.bd>
2022-02-09 18:39:40 +0600

    changed api path

--- services/auth/project/apis/__init__.py
+++ services/auth/project/apis/__init__.py
@@ -5,5 +5,5 @@ from project.apis.ping.api import ping_namespace
 
 api = Api(version="1.0", title="Users API", doc="/docs")
 
-api.add_namespace(ping_namespace, path="/api/v1/auth/ping")
-api.add_namespace(auth_namespace, path="/api/v1/auth")
+api.add_namespace(ping_namespace, path="/api/v1/auth-service/ping")
+api.add_namespace(auth_namespace, path="/api/v1/auth-service")
